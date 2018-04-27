a76c9bebfef9a75c1cea3cc06e823e1f234ea6a2
adapass182 <adampassingham90@gmail.com>
2018-04-27 21:34:12 +0200

    completed section one, declared variables for functions in section 2

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -1,5 +1,36 @@
 // Variables
 
+var hero = {
+  name: "Adam",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "bow",
+    damage: 100
+  }
+}
+
 // Game logic
+
+var rest = function () {
+
+}
+
+var pickUpItem = function () {
+
+}
+
+var dealDamage = function () {
+
+}
+
+var equipWeapon = function () {
+
+}
+
+var doBattle= function () {
+
+}
 
 // UI
